0ed785f2206e0c78a0df8371f51d06c4900ae322
kennedyifeadi <kennedyifeadi@gmail.com>
2025-02-20 20:22:52 +0100

    Refactor DarkModeContext to include DarkToggleProvider and update theme toggle

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,24 @@
+import { useContext } from "react"
 import { AiSummary } from "./components/AiSummary"
 import { Chart } from "./components/chart"
+import { DarkToggleContext} from "./components/context/DarkModeContext"
 import { Forcast } from "./components/Forcast"
 import { NavBar } from "./components/NavBar"
 import { ValueCard } from "./components/ValueCard"
 
 function App() {
-
+  const {isDark, setIsDark} = useContext(DarkToggleContext)
   return (
-    <div className="w-[100dvw] h-[100dvh] flex flex-col relative overflow-hidden">
-      <NavBar/>
-      <Forcast/>
-      <div>
-        <ValueCard/>
-        <ValueCard/>
-        <ValueCard/>
-      </div>
-      <Chart/>
-      <AiSummary/>
+    <div className={`w-[100dvw] h-[100dvh] flex flex-col relative overflow-hidden ${isDark ? "bg-black" : "bg-white"}`}>
+        <NavBar/>
+        <Forcast/>
+        <div>
+          <ValueCard/>
+          <ValueCard/>
+          <ValueCard/>
+        </div>
+        <Chart/>
+        <AiSummary/>
     </div>
   )
 }
