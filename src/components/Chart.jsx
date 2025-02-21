43352f40b8096dc9c29634ae683796f8157a2f5e
kennedyifeadi <kennedyifeadi@gmail.com>
2025-02-21 15:23:17 +0100

    Refactor Chart component to include dark mode support

--- src/components/Chart.jsx
+++ src/components/Chart.jsx
@@ -1,7 +1,31 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { DarkToggleContext } from './context/DarkModeContext'
 
 export const Chart = () => {
+  const {isDark} = useContext(DarkToggleContext)
+
   return (
-    <div>C</div>
+    <div className='w-full h-[32%] px-4 mb-4' >
+        <div className={`w-full h-full px-2 ${isDark ? "border-2 border-[#27272a] bg-[#27272ab9]" : "border-2 border-[#dbd9d99f] bg-[#dbd9d96c]"} rounded-md`}>
+            <div className='w-full flex justify-between'>
+                <div className='flex flex-col leading-0.5'>
+                    <span className={`text-xl font-bold ${isDark ? "text-[#fafafa]" : "text-[#09090b]"}`}>
+                        Chart Analysis
+                    </span>
+                    <span className='text-[9px] text-[#7f7f86]'>
+                        over the past week
+                    </span>
+                </div>
+                <div>
+                    <span>
+
+                    </span>
+                </div>
+            </div>
+            <div>
+                main Chart
+            </div>
+        </div>
+    </div>
   )
 }
