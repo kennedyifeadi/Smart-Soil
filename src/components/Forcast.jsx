d7e92711801000e35ba6bf7c571145bb7d5ca783
kennedyifeadi <kennedyifeadi@gmail.com>
2025-02-22 09:43:50 +0100

    Refactor Forcast component to include real-time weather data from API

--- src/components/Forcast.jsx
+++ src/components/Forcast.jsx
@@ -1,7 +1,47 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useContext, useEffect, useState } from 'react'
+import { DarkToggleContext } from './context/DarkModeContext'
 
 export const Forcast = () => {
+  const {isDark} = useContext(DarkToggleContext)
+  const [tomorrowForcast, setTomorrowForcastDay] = useState("")
+  const [todayForcast, setTodayForcastDay] = useState("")
+
+  const weatherAPI = "https://api.weatherapi.com/v1/forecast.json?key=a4f33c6a6aa4444a949135854241011&q=ibadan&days=2&aqi=yes&alerts=yes"
+
+
+  const fetchData = async ()=>{
+    const res = await axios.get(weatherAPI)
+    const data = res.data
+    const tomorrowForcast = data.forecast.forecastday[1].day.condition.text
+    const todayForcast = data.current.condition.text
+
+    setTomorrowForcastDay(tomorrowForcast)
+    setTodayForcastDay(todayForcast)
+}
+
+  useEffect(()=>{
+    fetchData()
+}, []);
+
   return (
-    <div className='w-full h-[5%]'>Forcast</div>
+    <div className='w-full h-[5%] flex gap-2 px-4 mb-2'>
+      <div className={`w-max h-max flex flex-col border-2 ${isDark ? "text-[#fafafa] border-[#27272a] bg-[#27272ab9]" : "text-[#09090b] border-[#dbd9d99f] bg-[#dbd9d96c]"} rounded-full px-4`}>
+        <span className='w-max text-[12px]'>
+          {todayForcast}
+        </span>
+        <span className={`text-[8px] ${isDark ? "text-[#cfdf32]" : "text-[#ffbc02]"}`}>
+          today's reading
+        </span>
+      </div>
+      <div className={`w-max h-max flex flex-col border-2 ${isDark ? "text-[#fafafa] border-[#27272a] bg-[#27272ab9]" : "text-[#09090b] border-[#dbd9d99f] bg-[#dbd9d96c]"} rounded-full px-4`}>
+        <span className='w-max text-[12px]'>
+          {tomorrowForcast}
+        </span>
+        <span className={`text-[8px] ${isDark ? "text-[#cfdf32]" : "text-[#ffbc02]"}`}>
+          tomorrow's reading
+        </span>
+      </div>
+    </div>
   )
 }
