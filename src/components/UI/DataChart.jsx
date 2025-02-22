5a78952559eceb15a15d8c479e636a8834fdf0f9
kennedyifeadi <kennedyifeadi@gmail.com>
2025-02-22 08:06:36 +0100

    Refactor DataChart component to include real-time data updates from Firebase

--- src/components/UI/DataChart.jsx
+++ src/components/UI/DataChart.jsx
@@ -1,8 +1,29 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import ApexCharts from "apexcharts";
+import { onValue, ref } from "firebase/database";
+import { db } from "../../firebase/firebase";
 
 export const DataChart = () => {
+  const [temperature, setTemperature] = useState(null);
+  const [moisture, setMoisture] = useState(null);
+  const [humidity, setHumidity] = useState(null);
+
   useEffect(() => {
+    const updateDashboard = () => {
+      onValue(ref(db, "Data"), (snapshot) => {
+        const Data = snapshot.val();
+        setTemperature(Data?.Temperature || 0);
+        setMoisture(Data?.Moisture || 0);
+        setHumidity(Data?.Humidity || 0);
+      });
+    };
+
+    updateDashboard();
+  }, []);
+
+  useEffect(() => {
+    if (humidity === null || temperature === null || moisture === null) return;
+
     const options = {
       colors: ["#1A56DB", "#FDBA8C"],
       series: [
@@ -11,7 +32,7 @@ export const DataChart = () => {
           color: "#ffbc02",
           data: [
             { x: "Mon", y: 200 },
-            { x: "Tue", y: 300 },
+            { x: "Tue", y: humidity },
             { x: "Wed", y: 163 },
             { x: "Thu", y: 21 },
             { x: "Fri", y: 162 },
@@ -24,7 +45,7 @@ export const DataChart = () => {
           color: "#1A56DB",
           data: [
             { x: "Mon", y: 231 },
-            { x: "Tue", y: 122 },
+            { x: "Tue", y: temperature },
             { x: "Wed", y: 63 },
             { x: "Thu", y: 421 },
             { x: "Fri", y: 122 },
@@ -37,7 +58,7 @@ export const DataChart = () => {
           color: "#FDBA8C",
           data: [
             { x: "Mon", y: 232 },
-            { x: "Tue", y: 113 },
+            { x: "Tue", y: moisture },
             { x: "Wed", y: 341 },
             { x: "Thu", y: 224 },
             { x: "Fri", y: 522 },
@@ -50,9 +71,7 @@ export const DataChart = () => {
         type: "bar",
         height: "100%",
         fontFamily: "Inter, sans-serif",
-        toolbar: {
-          show: false,
-        },
+        toolbar: { show: false },
       },
       plotOptions: {
         bar: {
@@ -65,40 +84,20 @@ export const DataChart = () => {
       tooltip: {
         shared: true,
         intersect: false,
-        style: {
-          fontFamily: "Inter, sans-serif",
-        },
+        style: { fontFamily: "Inter, sans-serif" },
       },
       states: {
-        hover: {
-          filter: {
-            type: "darken",
-            value: 1,
-          },
-        },
+        hover: { filter: { type: "darken", value: 1 } },
       },
-      stroke: {
-        show: true,
-        width: 0,
-        colors: ["transparent"],
-      },
+      stroke: { show: true, width: 0, colors: ["transparent"] },
       grid: {
         show: false,
         strokeDashArray: 4,
-        padding: {
-          left: 2,
-          right: 2,
-          top: -20,
-        },
+        padding: { left: 2, right: 2, top: -20 },
       },
-      dataLabels: {
-        enabled: false,
-      },
-      legend: {
-        show: false,
-      },
+      dataLabels: { enabled: false },
+      legend: { show: false },
       xaxis: {
-        floating: false,
         labels: {
           show: true,
           style: {
@@ -106,19 +105,11 @@ export const DataChart = () => {
             cssClass: "text-xs font-normal fill-gray-500 dark:fill-gray-400",
           },
         },
-        axisBorder: {
-          show: false,
-        },
-        axisTicks: {
-          show: false,
-        },
+        axisBorder: { show: false },
+        axisTicks: { show: false },
       },
-      yaxis: {
-        show: false,
-      },
-      fill: {
-        opacity: 1,
-      },
+      yaxis: { show: false },
+      fill: { opacity: 1 },
     };
 
     const chartElement = document.getElementById("column-chart");
@@ -126,8 +117,7 @@ export const DataChart = () => {
       const chart = new ApexCharts(chartElement, options);
       chart.render();
     }
-  }, []);
+  }, [humidity, temperature, moisture]); 
 
   return <div className="w-full h-full" id="column-chart"></div>;
 };
-
